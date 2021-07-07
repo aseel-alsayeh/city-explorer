d89c619561146a9241f58a549c9d3cd321e4acf4
aseel-alsayeh <aseelalsayeh97@gmail.com>
2021-07-07 23:20:43 +0300

    final edit

--- src/App.js
+++ src/App.js
@@ -29,23 +29,34 @@ class App extends Component {
     event.preventDefault();
     
     let axiosResponse = await axios.get(`https://eu1.locationiq.com/v1/search.php?key=pk.d4d33965e53e9039815074b0322a83e1&q=${this.state.displayName}&format=json`)
-    let axiosLocalApi= await axios.get(`http://localhost:8080/weather?lat=31.95&lon=35.91&searchQuery=amman`)
+    
     
     console.log('hello')
     this.setState({
       
-      longitude: axiosResponse.data[0].lon,
-      latitude: axiosResponse.data[0].lat,
+      longitude: Number(axiosResponse.data[0].lon),
+      latitude: Number(axiosResponse.data[0].lat),
       searchQuery:axiosResponse.data[0].searchQuery,
-      weatherData:axiosLocalApi.data
       
     })
-    console.log(axiosLocalApi.data , typeof axiosLocalApi.data )
-    console.log(this.state.latitude)
-    console.log('axiosR',axiosResponse.data[0].lat)
+    
+    
+    
+    
+    let axiosLocalApi= await axios.get(`http://localhost:8080/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&searchQuery=${this.state.displayName}`)
+    this.setState({
+      
+      weatherData:axiosLocalApi.data,
+      // longitude: Number(axiosLocalApi.data[0].lon),
+      // latitude: Number(axiosLocalApi.data[0].lat),
+      longitude: Number(axiosResponse.data[0].lon),
+      latitude: Number(axiosResponse.data[0].lat),
+  
 
-
-
+    })
+    console.log(axiosLocalApi.data[0])
+    console.log(axiosLocalApi.data[0])
+    console.log(this.state.longitude,this.state.latitude)
   }
   
 
@@ -59,9 +70,6 @@ class App extends Component {
           <input type="text" placeholder="city name" onChange={(event) => { this.inputChange(event) }} />
           <button onClick={this.clickMe}> Explore </button>
         </form>
-        {/* <p> longitude : {this.state.longitude}</p> */}
-        {/* <p> latitude : {this.state.latitude}</p> */}
-        {console.log(this.state.displayName)}
       
         <Card style={{ width: '18rem' }}
         >
